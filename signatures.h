7bacdd999aff94a9f0da26377da0cdb373454d98
R4MZEZ <kazachenko2003@bk.ru>
2022-10-17 21:55:13 +0300

    struct signatures

---
+++ signatures.h
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <inttypes.h>
+
+struct tree_subheader {
+    char ASCII_signature[8];
+    uint64_t root_offset;
+    uint64_t first_seq;
+    uint64_t second_seq;
+    uint64_t cur_id;
+    uint64_t reserved[16];
+};
+
+struct pattern {
+    uint32_t size;
+    char type[4];
+    char *key;
+};
+
+
+struct tree_header {
+    struct tree_subheader subheader;
+    struct pattern *patterns;
+    uint64_t *offset;
+};
+
+/*
+ * Юнионы чтобы в зависимости от типа тапла
+ * там лежало либо одно, либо второе
+ * но места всегда занимает 16 байт
+ */
+struct tuple_header {
+    union {
+        struct {
+            uint64_t parent;
+            uint64_t alloc;
+        } node_header;
+
+        struct {
+            uint64_t prev;
+            uint64_t next;
+        } string_header;
+
+    } inner;
+};
+
+struct tuple {
+    struct tuple_header header;
+    int64_t *data;
+};
+
+struct document_tree {
+    struct tree_header header;
+    struct tuple* tuples;
+};
+
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
