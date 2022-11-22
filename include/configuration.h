9a6bc472578b6bf381f84593872fd76ffa96ac0b
mike <311765@niuitmo.ru>
2022-11-23 00:24:54 +0300

    changed configuration

--- include/configuration.h
+++ include/configuration.h
@@ -1,20 +1,28 @@
 #ifndef CONFIGURATION_H
 #define CONFIGURATION_H
+
 #define FILE_GRANULARITY 4
-#define MINIMAL_TUPLE_SIZE 256
-#define SINGLE_TUPLE_VALUE_SIZE 8
-#define OFFSET_VALUE_SIZE 8
-#define MIN_ID_ARRAY_SIZE 50000
-#define NULL_VALUE 0
 #define BIG_ENDIAN_SIGNATURE 0xFFFE
-#define PRINT_ID_ARRAY_LEN 16
+/* size of one value in tuple [bytes] */
+#define SINGLE_TUPLE_VALUE_SIZE 8
+/* size of offset in tuple [bytes] */
+#define OFFSET_VALUE_SIZE 8
+/* minimal id array size [tuple sizes] */
+#define MIN_ID_ARRAY_SIZE 50000
+#define MINIMAL_TUPLE_SIZE 256
+/* size of string to read command */
+#define BUFFER_COMMAND_SIZE 256
+/* size of string to read field value */
+#define BUFFER_FIELD_SIZE 1024
+
+/* default value to fill empty containers */
+#define NULL_VALUE 0
+
+/* basic types that can contains in tuple  */
 #define BOOLEAN_TYPE 0
 #define INTEGER_TYPE 1
 #define FLOAT_TYPE 2
 #define STRING_TYPE 3
-#define MAGIC_NUMBER 42
-#define BUFFER_COMMAND_SIZE 256
-#define BUFFER_FIELD_SIZE 1024
 
 /* Prime numbers to hash */
 #define AHASH 54059
